47eca775a1f9088b0d7eff82868631108edaadbb
Sxmourai <samourai_x@outlook.fr>
2023-07-01 21:05:25 +0200

    Finished implementing heap allocator, tests, bump and linked list allocation

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,4 +1,6 @@
-
+//pub mod bump;
+//pub mod linked_list;
+pub mod fixed_size_block;
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 use x86_64::{
@@ -7,12 +9,12 @@ use x86_64::{
     },
     VirtAddr,
 };
-use linked_list_allocator::LockedHeap;
+use self::fixed_size_block::FixedSizeBlockAllocator;
 
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(
+    FixedSizeBlockAllocator::new());
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
@@ -55,4 +57,29 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
 }
