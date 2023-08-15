be232a41c9ebf6190d7401f4efb8ff0121720ea9
Sxmourai <samourai_x@outlook.fr>
2023-08-15 08:27:02 +0200

    Added colors in logging

--- src/boot.rs
+++ src/boot.rs
@@ -1,13 +1,14 @@
 use core::ffi::c_uchar;
 
 use alloc::{vec::{Vec, self}, string::String, format};
+use ::log::trace;
 use x86_64::{VirtAddr, structures::paging::{PhysFrame, Mapper, Page, PageTableFlags, Size4KiB}, PhysAddr};
 
 use crate::{
     gdt, interrupts, serial_println, state::{self, STATE},
     task::executor::Executor,
     writer::{inb, outb, print_at},
-    MemoryHandler, memory::{read_phys_memory_and_map, rsdp::DescriptorTablesHandler, self}, serial_print, log,
+    MemoryHandler, memory::{read_phys_memory_and_map, rsdp::DescriptorTablesHandler, self}, serial_print, log::{self, initialize_logger},
 };
 // Supress compiler warning about unused imports, but if removed, error
 #[allow(unused_imports)]
@@ -16,10 +17,11 @@ use crate::println;
 
 // Boot the os, with the help of the 'boot_info' provided by the bootloader crate
 pub fn boot(boot_info: &'static bootloader::BootInfo) {
+    initialize_logger();
     //TODO Can't use vecs, Strings before heap init (in memoryHandler init), which means we can't do trace... Use a constant-size list ?
-    // trace!("Initializing GDT");
+    trace!("Initializing GDT");
     gdt::init();
-    // trace!("Initializing Interrupts & CPU exceptions");
+    trace!("Initializing Interrupts & CPU exceptions");
     interrupts::init();
 
     let memory_handler = MemoryHandler::new(
--- src/log.rs
+++ src/log.rs
@@ -3,9 +3,46 @@ use spin::Mutex;
 
 use crate::{serial_println, println, serial_print};
 use lazy_static::lazy_static;
-use log::{Level, LevelFilter, Log, Metadata, Record};
+use log::{Level, LevelFilter, Log, Metadata, Record, error};
+
+//TODO Have all codes https://chrisyeh96.github.io/2020/03/28/terminal-colors.html
+enum Color {
+    Black = 0,
+    Red = 1,
+    Green = 2,
+    Yellow = 3,
+    Blue = 4,
+    Magenta = 5,
+    Cyan = 6,
+    White = 7,
+}
+impl From<u8> for Color {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Color::Black,
+            1 => Color::Red,
+            2 => Color::Green,
+            3 => Color::Yellow,
+            4 => Color::Blue,
+            5 => Color::Magenta,
+            6 => Color::Cyan,
+            7 => Color::White,
+            _ => {error!("From u8 but u8 is too big !");Color::White}
+        }
+    }
+}
+enum Codes {
+    Reset = 0,
+    Bold = 1,
+    Dim = 2,
+    Underline = 4,
+    SlowBlink = 5,
+}
+impl Codes {
+    fn reset() -> &'static str {"\x1b[0;0m"}
+}
+
 struct Logger;
-const MAX_LEVEL: Level = Level::Trace;
 impl Log for Logger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         // Implement your own logic to determine if the log level is enabled
@@ -14,7 +51,8 @@ impl Log for Logger {
     fn log(&self, record: &Record) {
         // Implement your own logic to handle the log record
         let mut buffer = [0u8; 128];
-        let _ = serial_println!("[{}] {}", record.level(), record.args());
+        let color = format!("\x1b[1;3{}m", Color::from(record.level() as u8) as u8);
+        serial_println!("[{}{}{}] {}", color, record.level(),Codes::reset(), record.args());
         // Your logic to write to an output (e.g., a memory buffer)
     }
     //TODO
@@ -23,7 +61,12 @@ impl Log for Logger {
         // Implement your own logic to flush the log messages
     }
 }
-
+const MAX_LEVEL: Level = Level::Trace;
+pub fn initialize_logger() {
+    static LOGGER: Logger = Logger;
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(LevelFilter::Debug);
+}
 
 
 
