0b4813530978199e76b40e5351cfd792a43d19df
Sxmourai <samourai_x@outlook.fr>
2024-01-14 09:48:49 +0100

    fix: removed useless get_pcis in build/ because using update version of it

--- build/get_pci.sh
+++
@@ -1,5 +0,0 @@
-rm pci.ids -f
-wget https://pci-ids.ucw.cz/v2.2/pci.ids.xz
-xz -d pci.ids.xz
-rm pci.ids.xz -f
-python3 pci_ids_fmt.py
--- build/pci_ids_fmt.py
+++
@@ -1,6 +0,0 @@
-with open("pci.ids", mode="r") as f:
-    raw_lines = f.readlines()
-    lines = list(filter(lambda line: not line.startswith("#") and line.strip()!="", raw_lines))
-
-with open("pci.ids", mode="w") as f:
-    f.writelines(lines)
